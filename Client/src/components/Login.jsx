96dae9de5daa587c5003feb27141bf1e8f08a970
Dunadunn <alxrodrig7@gmail.com>
2023-11-15 21:34:26 -0800

    made changes to have frontend mutations to match backend

--- Client/src/components/Login.jsx
+++ Client/src/components/Login.jsx
@@ -1,23 +1,56 @@
 import React, { useState } from 'react';
+import { useMutation, gql } from '@apollo/client';
 
-const Login = ({ onLogin }) => {
-  const [username, setUsername] = useState('');
+// Define the GraphQL login mutation matching the backend
+const LOGIN_MUTATION = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+const Login = ({ onLoginSuccess }) => {
+  // States for email and password
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleSubmit = (event) => {
+  // Use the login mutation
+  const [login, { loading, error }] = useMutation(LOGIN_MUTATION);
+
+  // Form submission handler
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    onLogin(username, password);
+    try {
+      // Execute the login mutation
+      const response = await login({
+        variables: {
+          email: email,
+          password: password
+        }
+      });
+
+      // Invoke the callback with the token on successful login
+      onLoginSuccess(response.data.login.token);
+    } catch (error) {
+      // Log the error or update the UI to inform the user
+      console.error('Login error', error);
+    }
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <h2>Login</h2>
       <div>
-        <label>Username:</label>
+        <label>Email:</label>
         <input 
-          type="text" 
-          value={username} 
-          onChange={(e) => setUsername(e.target.value)}
+          type="email" 
+          value={email} 
+          onChange={(e) => setEmail(e.target.value)}
           required
         />
       </div>
@@ -30,7 +63,8 @@ const Login = ({ onLogin }) => {
           required
         />
       </div>
-      <button type="submit">Login</button>
+      <button type="submit" disabled={loading}>Login</button>
+      {error && <p>Error in login: {error.message}</p>}
     </form>
   );
 };
