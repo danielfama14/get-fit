bde9f06839d0db1111b6570dab42ed3de5e957cb
Gustavo Martinez <gmtz.0794@gmail.com>
2023-11-20 18:13:55 -0800

    added pages, components, and modified src files

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,71 +1,52 @@
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import LoginPage from './pages/LoginPage';
-
-import WaterIntakeTracker from './WaterIntakeTracker'; // Add this import
-import StepCounter from './StepCounter'; // Add this import
-import WorkoutStatistics from './WorkoutStatistics'; // Add this import
-
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { Outlet } from 'react-router-dom';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+import LoginPage from './pages/LoginPage';
+import Login from './components/Login';
+
+// Construct our main GraphQL API endpoint
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
 
 const client = new ApolloClient({
-  uri: '/graphql',
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <ApolloProvider client={client}>
-    <Router>
-      <div>
-        <nav>
-          <Link to="/login">Login</Link>
-          <Link to="/water">Water Intake</Link>
-          <Link to="/steps">Step Counter</Link>
-          <Link to="/statistics">Workout Statistics</Link>
-        </nav>
-
-        <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/water" element={<WaterIntakeTracker />} />
-          <Route path="/steps" element={<StepCounter />} />
-          <Route path="/statistics" element={<WorkoutStatistics />} />
-          <Route
-            path="/"
-            element={
-              <>
-                <div>
-                  <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                  </a>
-                  <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                  </a>
-                </div>
-                <h1>Vite + React</h1>
-                <div className="card">
-                  <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                  </button>
-                  <p>
-                    Edit <code>src/App.jsx</code> and save to test HMR
-                  </p>
-                </div>
-                <p className="read-the-docs">
-                  Click on the Vite and React logos to learn more
-                </p>
-              </>
-            }
-          />
-        </Routes>
+      <div className="flex-column justify-flex-start min-100-vh">
+        <Header />
+        <div className="container">
+          <Outlet />
+        </div>
+        <Footer />
       </div>
-      </Router>
     </ApolloProvider>
   );
 }
--- Client/src/pages/LoginPage.jsx
+++ Client/src/pages/LoginPage.jsx
@@ -1,18 +1,65 @@
-import React from 'react';
-import Login from '../components/Login';
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
 
-const LoginPage = () => {
-  const handleLogin = (username, password) => {
-    console.log('Login attempted with:', username, password);
-    // Here you can add logic for handling the login, e.g., verifying credentials, setting state, etc.
+const Login = ({ onLoginSuccess }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  // Use the LOGIN_USER mutation
+  const [loginUser] = useMutation(LOGIN_USER);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      setLoading(true);
+
+      // Execute the LOGIN_USER mutation
+      const { data } = await loginUser({
+        variables: {
+          email,
+          password,
+        },
+      });
+
+      // Invoke the callback with the token on successful login
+      onLoginSuccess(data.login.token);
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
-    <div>
-      <h1>Welcome to the Login Page</h1>
-      <Login onLogin={handleLogin} />
-    </div>
+    <form onSubmit={handleSubmit}>
+      <h2>Login</h2>
+      <div>
+        <label>Email:</label>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </div>
+      <div>
+        <label>Password:</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </div>
+      <button type="submit" disabled={loading}>
+        Login
+      </button>
+      {error && <p>Error in login: {error}</p>}
+    </form>
   );
 };
 
-export default LoginPage;
+export default Login;
